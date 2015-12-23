954906ba388a1baf88301816543d6ce0583515ab
Luca Rebuffi <luca.rebuffi@elettra.eu>
2015-12-23 11:48:05 +0100

    Setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 from setuptools import find_packages, setup
 
 NAME = 'XOPPY-Devel'
-VERSION = '0.0.7'
+VERSION = '0.0.8'
 ISRELEASED = False
 
 DESCRIPTION = 'XOPPY: XOP (X-ray oriented programs) in Python'
