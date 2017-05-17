a4048ae1a3a47cb72145da93fdd429aee4f8c29f
Luca Rebuffi <luca.rebuffi@elettra.eu>
2017-05-17 17:40:38 +0200

    Oasys 1.0 Release

--- setup.py
+++ setup.py
@@ -4,10 +4,6 @@ import os
 
 try:
     from setuptools import find_packages, setup
-except ImportError:
-    import ez_setup
-    ez_setup.use_setuptools()
-    from setuptools import find_packages, setup
 except AttributeError:
     from setuptools import find_packages, setup
 
