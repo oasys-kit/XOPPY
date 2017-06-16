6a1783b94e2edbba7b51901d287d2eaf3c19b53e
Manuel Sanchez del Rio <srio@esrf.eu>
2017-06-16 16:09:33 +0200

    changed for silx 0.5.0

--- orangecontrib/xoppy/util/xoppy_util.py
+++ orangecontrib/xoppy/util/xoppy_util.py
@@ -103,15 +103,13 @@ class XoppyPlot:
     @classmethod
     def plot_histo(cls, plot_window, x, y, title, xtitle, ytitle, color='blue', replace=True):
         matplotlib.rcParams['axes.formatter.useoffset']='False'
-
         plot_window.addCurve(x, y, title, symbol='', color=color, xlabel=xtitle, ylabel=ytitle, replace=replace) #'+', '^', ','
-
         if not xtitle is None: plot_window.setGraphXLabel(xtitle)
         if not ytitle is None: plot_window.setGraphYLabel(ytitle)
         if not title is None: plot_window.setGraphTitle(title)
 
         plot_window.setDrawModeEnabled(True, 'rectangle')
-        plot_window.setInteractiveMode(True)
+        plot_window.setInteractiveMode('zoom',color='orange')
         plot_window.resetZoom()
         plot_window.replot()
 
