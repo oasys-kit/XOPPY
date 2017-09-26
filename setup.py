4c8d8e23f995950b16ed8f7aa16536285a145b0f
luca.rebuffi@elettra.eu <Bacilotto_00>
2017-09-26 11:48:25 +0200

    Bug Fixing

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except AttributeError:
     from setuptools import find_packages, setup
 
 NAME = 'OASYS1-XOPPY'
-VERSION = '1.0.27'
+VERSION = '1.0.28'
 ISRELEASED = False
 
 DESCRIPTION = 'XOPPY: XOP (X-ray oriented programs) in Python'
@@ -40,7 +40,7 @@ SETUP_REQUIRES = (
 )
 
 INSTALL_REQUIRES = (
-    'oasys1>=1.0.13',
+    'oasys1>=1.0.18',
     'pySRU',
 )
 
