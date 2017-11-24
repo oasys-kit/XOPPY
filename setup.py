d16a8f8542a7cd5039e18338934d4e721332894e
Luca Rebuffi <luca.rebuffi@elettra.eu>
2017-11-24 14:51:23 +0100

    SRW lib compiled libraries

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except AttributeError:
     from setuptools import find_packages, setup
 
 NAME = 'OASYS1-XOPPY'
-VERSION = '1.0.32'
+VERSION = '1.0.33'
 ISRELEASED = False
 
 DESCRIPTION = 'XOPPY: XOP (X-ray oriented programs) in Python'
@@ -41,6 +41,7 @@ SETUP_REQUIRES = (
 
 INSTALL_REQUIRES = (
     'oasys1>=1.0.18',
+    'oasys1-srwlib',
     'pySRU',
 )
 
@@ -99,36 +100,3 @@ if __name__ == '__main__':
               )
 
         if is_beta: raise NotImplementedError("This version of XOPPY doesn't work with Oasys1 beta.\nPlease install OASYS1 final release: http://www.elettra.eu/oasys.html")
-
-
-        try:
-            is_install = False
-            for arg in sys.argv:
-                if arg == 'install': is_install = True
-
-            if is_install:
-                site_packages_dir = None
-
-                for directory in site.getsitepackages():
-                    if os.path.exists(directory + "/oasys"):
-                        site_packages_dir = directory
-                        break
-
-                if not site_packages_dir is None:
-                    if sys.platform == 'darwin':
-                        shutil.copyfile("libraries/" + str(sys.platform) + "/srwl_bl.py", site_packages_dir + "/srwl_bl.py")
-                        shutil.copyfile("libraries/" + str(sys.platform) + "/srwl_uti_cryst.py", site_packages_dir + "/srwl_uti_cryst.py")
-                        shutil.copyfile("libraries/" + str(sys.platform) + "/srwl_uti_src.py", site_packages_dir + "/srwl_uti_src.py")
-                        shutil.copyfile("libraries/" + str(sys.platform) + "/srwl_util_und.py", site_packages_dir + "/srwl_util_und.py")
-                        shutil.copyfile("libraries/" + str(sys.platform) + "/srwlib.py", site_packages_dir + "/srwlib.py")
-                        shutil.copyfile("libraries/" + str(sys.platform) + "/uti_math.py", site_packages_dir + "/uti_math.py")
-                        shutil.copyfile("libraries/" + str(sys.platform) + "/srwlpy.so", site_packages_dir + "/srwlpy.so")
-                    elif sys.platform == 'linux':
-                        pass
-
-                    # TODO: to be removed
-                    #if sys.platform == 'linux':
-                    #    os.remove(site_packages_dir + "/orangecontrib/xoppy/widgets/optics/xpower.py")
-
-        except Exception as exception:
-            print(str(exception))
