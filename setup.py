6928329ed84607d8bf0ec26c268a3d8a41b7e6be
lucarebuffi <lrebuffi@anl.gov>
2020-01-06 14:57:15 -0600

    PyQt 5.14 bug fixed

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except AttributeError:
     from setuptools import find_packages, setup
 
 NAME = 'OASYS1-XOPPY'
-VERSION = '1.0.63'
+VERSION = '1.0.64'
 ISRELEASED = False
 
 DESCRIPTION = 'XOPPY: XOP (X-ray oriented programs) in Python'
@@ -40,7 +40,7 @@ SETUP_REQUIRES = (
 )
 
 INSTALL_REQUIRES = (
-    'oasys1>=1.2.26',
+    'oasys1>=1.2.27',
     'oasys-srwpy',
     'pySRU',
 )
