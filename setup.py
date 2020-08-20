611b56c1b723d700629ddab511ebb5f56eb7001e
lucarebuffi <lrebuffi@anl.gov>
2020-08-20 16:38:36 -0500

    New Developments

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except AttributeError:
     from setuptools import find_packages, setup
 
 NAME = 'OASYS1-XOPPY'
-VERSION = '1.0.88'
+VERSION = '1.0.89'
 ISRELEASED = False
 
 DESCRIPTION = 'XOPPY: XOP (X-ray oriented programs) in Python'
@@ -40,7 +40,7 @@ SETUP_REQUIRES = (
 )
 
 INSTALL_REQUIRES = (
-    'oasys1>=1.2.43',
+    'oasys1>=1.2.45',
     'oasys-srwpy',
     'pySRU',
     'srxraylib>=1.0.30',  # remove when oasys1 requires at least this version
