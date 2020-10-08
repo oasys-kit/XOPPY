6194db5598879ea828b975abb8a3d95a19a2d9ee
Manuel Sanchez del Rio <srio@esrf.eu>
2020-10-08 21:03:53 +0200

    recompiled a few apps

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except AttributeError:
     from setuptools import find_packages, setup
 
 NAME = 'OASYS1-XOPPY'
-VERSION = '1.0.92'
+VERSION = '1.0.93'
 ISRELEASED = False
 
 DESCRIPTION = 'XOPPY: XOP (X-ray oriented programs) in Python'
