b61e2f529901fce30dd8a58e7dc638706977cf99
Manuel Sanchez del Rio <srio@esrf.eu>
2024-01-05 15:20:10 +0100

    align xoppylib 1.0.22

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except AttributeError:
     from setuptools import find_packages, setup
 
 NAME = 'OASYS1-XOPPY'
-VERSION = '1.2.12'
+VERSION = '1.2.13'
 ISRELEASED = False
 
 DESCRIPTION = 'XOPPY: XOP (X-ray oriented programs) in Python'
@@ -44,7 +44,7 @@ INSTALL_REQUIRES = (
     'oasys-srwpy',
     'pySRU',
     'srxraylib',
-    'xoppylib>=1.0.21',
+    'xoppylib>=1.0.22',
 )
 
 PACKAGES = find_packages(exclude=('*.tests', '*.tests.*', 'tests.*', 'tests'))
