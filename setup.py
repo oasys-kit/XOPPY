6dbfd75f4ba884e33eadad4b93b89d36548161f1
Manuel Sanchez del Rio <srio@esrf.eu>
2025-10-23 13:26:04 +0200

    1.2.27

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except AttributeError:
     from setuptools import find_packages, setup
 
 NAME = 'OASYS1-XOPPY'
-VERSION = '1.2.27'
+VERSION = '1.2.28'
 ISRELEASED = False
 
 DESCRIPTION = 'XOPPY: XOP (X-ray oriented programs) in Python'
